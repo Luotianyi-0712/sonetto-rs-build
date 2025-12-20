43ff908f747f190b6f52aebcdf63cca0915f65da
yoncodes <jammie080@gmail.com>
2025-12-19 23:41:57 -0500

    added readme

---
+++ README.rs
@@ -0,0 +1,38 @@
+##sonetto-rs
+
+## What is sonetto-rs?
+
+sonetto-rs is a ps for reverse 1999 made in rust. why? no one tried and succeeded yet.
+
+## How to use sonetto-rs?
+- need to install rust and compile it with cargo
+```bash
+cargo build --release
+```
+- need to use the sonetto patch (soon)
+
+## Features
+Everythings unlocked out the box
+- All skins
+- All heroes
+- All Psychubes
+- 3m currency to start
+- Battles work (kinda)
+- Battle replay works
+- Battle teams can be set and saved now
+- Username changes work
+- Users can change the profile heros
+- Users can change Psychubes on heros
+
+## Not working (confirmed)
+- Tower battles
+- Gacha (soon)
+- Setting hero talents
+- Achievements
+- Tasks
+- Battle pass
+- Stories (not tested)
+- Currency logic (soon)
+- Profile picture (soon)
+- Real battle logic (right now we skip battle to the end)
+- Auto battle (soon)
